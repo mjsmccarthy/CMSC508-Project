5dd0bc18aeea91605117469aadcb7eb96485ab04
Matthew McCarthy <mjsmccarthy@gmail.com>
2019-11-19 16:40:49 -0500

    Minor updates

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # [START gae_python37_cloudsql_mysql_pooling]
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import sqlalchemy
+from sqlalchemy import exc
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+db_user = 'client'
+db_password = 'cmsc508_project'
+db_name = 'project'
+db_connection_name = 'cmsc508-project-258320:us-central1:myinstance'
 
 # When deployed to App Engine, the `GAE_ENV` environment variable will be
 # set to `standard`
@@ -22,12 +23,13 @@ else:
     # so that your application can use 127.0.0.1:3306 to connect to your
     # Cloud SQL instance
     host = '127.0.0.1'
-    engine_url = 'mysql+pymysql://{}:{}@{}/{}'.format(
-        db_user, db_password, host, db_name)
+    port = 3308
+    engine_url = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
+        db_user, db_password, host, port, db_name)
 
-# The Engine object returned by create_engine() has a QueuePool integrated
-# See https://docs.sqlalchemy.org/en/latest/core/pooling.html for more
-# information
+# # The Engine object returned by create_engine() has a QueuePool integrated
+# # See https://docs.sqlalchemy.org/en/latest/core/pooling.html for more
+# # information
 engine = sqlalchemy.create_engine(engine_url, pool_size=3)
 
 app = Flask(__name__)
@@ -38,18 +40,21 @@ def form():
     return render_template('form.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def form_post():
     text = request.form['text']
     cnx = engine.connect()
-    cursor = cnx.execute(text)
+    try:
+        cursor = cnx.execute(text)
+    except exc.OperationalError as err:
+        return render_template('form.html', err=err)
     result = cursor.fetchall()
     columns = result[0].keys()
     # If the connection comes from a pool, close() will send the connection
     # back to the pool instead of closing it
     cnx.close()
-
     return render_template('form.html', result=result, columns=columns)
+    
 # [END gae_python37_cloudsql_mysql_pooling]
 
 
